463ea0a0a7bf11a39a1a64983dc05f6958f758c4
Abdullah9715 <muftimuhammadabdullah225@gmail.com>
2024-01-27 06:13:06 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/navbar/Navbar';
+import Footer from './components/footer/Footer';
+import Home from './pages/home/Home';
+import Index from './routes/Routes';
+
+function App() {
+  return (<>
+ <Index/>
+  </>
+  );
+}
+
+export default App;
